e70a1bd30bf3f0a4ea198c012fac97c61ab8ecd5
saquibe <saquibe143@gmail.com>
2025-01-21 18:43:37 +0530

    unique category

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import Title from "./Title";
 import menu from "./data";
 import Menu from "./Menu";
 
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
+
 function App() {
   const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
   return (
     <main>
       <section className="menu">
